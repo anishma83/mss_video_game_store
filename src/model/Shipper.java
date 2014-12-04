2601e96f9c47b9b9144f683971536ac58d3d8f99
Kyle Bissell <kbissell@miraclesoft.com>
2014-12-04 14:30:43 -0500

    Updated Shipper

--- src/model/Shipper.java
+++ src/model/Shipper.java
@@ -1,9 +1,38 @@
 package com.mss.store.videogame.model;
 
-/**
-* Author:kjbissell
-*/
 public class Shipper {
+	private int 	shipper_Id;
+	private String 	shipper_Name;
+	private String 	phone;
+	
+	public Shipper()
+	{
+		
+	}
+
+	public int getShipper_Id() {
+		return shipper_Id;
+	}
+
+	public void setShipper_Id(int shipper_Id) {
+		this.shipper_Id = shipper_Id;
+	}
+
+	public String getShipper_Name() {
+		return shipper_Name;
+	}
+
+	public void setShipper_Name(String shipper_Name) {
+		this.shipper_Name = shipper_Name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
 
 }
-
