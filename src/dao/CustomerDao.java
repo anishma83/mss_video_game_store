4db771ebd53c5ce8cd1c2a6304f3bdbf5739ad3c
rizaerbas <rizaerbas@gmail.com>
2014-12-30 11:30:04 -0500

    updated customerdao and customerdaoimpl

--- src/dao/CustomerDao.java
+++ src/dao/CustomerDao.java
@@ -2,16 +2,22 @@ package com.mss.store.videogame.dao;
 
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import com.mss.store.videogame.model.Customer;
 
 public interface CustomerDao {
-	
+
 	public void save(Customer customer);
+
+	public void updateCustomer(Customer customer);
+
+	public Customer merge(Customer customer);
+
 	public List<Customer> list();
+
 	public List<Customer> lookupById(int customerId);
+
 	public List<Customer> lookupByEmail(String email);
+
 	public Customer signedIn(String form_email);
 
 }
--- src/dao/CustomerDaoImpl.java
+++ src/dao/CustomerDaoImpl.java
@@ -1,15 +1,9 @@
 package com.mss.store.videogame.dao;
 
-
 import java.util.List;
-
-import javax.persistence.Query;
-import javax.servlet.http.HttpSession;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.springframework.orm.hibernate4.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,24 +14,18 @@ import com.mss.store.videogame.model.Customer;
 @Service
 @Repository
 public class CustomerDaoImpl implements CustomerDao {
-	
+
 	private SessionFactory sessionfactory;
 
-
-	public void setSessionFactory(SessionFactory sessionfactory){
-		this.sessionfactory=sessionfactory;
-	}
-	
-	private Session getCurrentSession() {
-		return sessionfactory.getCurrentSession();
+	public void setSessionFactory(SessionFactory sessionfactory) {
+		this.sessionfactory = sessionfactory;
 	}
 
 	@Override
 	@Transactional
 	public void save(Customer customer) {
-		System.out.println(customer.toString());
 		sessionfactory.getCurrentSession().save(customer);
-		
+
 	}
 
 	@SuppressWarnings("unchecked")
@@ -45,8 +33,8 @@ public class CustomerDaoImpl implements CustomerDao {
 	@Transactional
 	public List<Customer> list() {
 		List<Customer> customers;
-		Session session=this.sessionfactory.getCurrentSession();
-		customers = (List<Customer>) session.createQuery("from Customer").list();
+		Session session = this.sessionfactory.getCurrentSession();
+		customers = session.createQuery("from Customer").list();
 		return customers;
 	}
 
@@ -55,8 +43,9 @@ public class CustomerDaoImpl implements CustomerDao {
 	@Transactional
 	public List<Customer> lookupById(int customerId) {
 		List<Customer> customers;
-		Session session=this.sessionfactory.getCurrentSession();
-		customers = (List<Customer>) session.createQuery("from Customer where customerID="+customerId).list();
+		Session session = this.sessionfactory.getCurrentSession();
+		customers = session.createQuery(
+				"from Customer where customerID=" + customerId).list();
 		return customers;
 	}
 
@@ -65,24 +54,45 @@ public class CustomerDaoImpl implements CustomerDao {
 	@Transactional
 	public List<Customer> lookupByEmail(String email) {
 		List<Customer> customers;
-		Session session= this.sessionfactory.getCurrentSession();
-		customers = (List<Customer>) session.createQuery("from Customer where email="+email).list();
+		Session session = this.sessionfactory.getCurrentSession();
+		customers = session.createQuery("from Customer where email=" + email)
+				.list();
 		return customers;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	@Transactional
 	public Customer signedIn(String email) {
 		List<Customer> customerList = null;
 		Customer customer = null;
-		Session session= this.sessionfactory.getCurrentSession(); 
-		customerList = (List<Customer>) session.createQuery("from Customer where email= :email ").setParameter("email", email).list();
-		if(customerList.size()>0)
-		{
-			System.out.println("The information retrieved from the database is: "+customerList.get(0).toString());
+		Session session = this.sessionfactory.getCurrentSession();
+		customerList = session
+				.createQuery("from Customer where email= :email ")
+				.setParameter("email", email).list();
+		if (customerList.size() > 0) {
 			customer = customerList.get(0);
 		}
 		return customer;
 	}
-	
+
+	@Override
+	@Transactional
+	public void updateCustomer(Customer customer) {
+
+		System.out.println("the values passed " + customer.toString());
+
+		// customer= (Customer)
+		// sessionfactory.getCurrentSession().merge(customer);
+		sessionfactory.getCurrentSession().update(customer);
+
+	}
+
+	@Override
+	public Customer merge(Customer customer) {
+		Customer cust = (Customer) sessionfactory.getCurrentSession().merge(
+				customer);
+		return cust;
+	}
+
 }
